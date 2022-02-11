d05b57af5df8efc6dd7de29190dfefddd8aececa
Kayla Durrett <kayla.durrett@gmail.com>
2022-02-11 10:46:00 -0700

    Add main page with basic styling

--- src/App.js
+++ src/App.js
@@ -1,1 +1,25 @@
+import React, { Component } from 'react'
+import './App.css'
 
+class App extends Component {
+  constructor(){
+    super()
+  }
+
+  render() {
+    return (
+      <main className='app'>
+        <nav className='nav-bar'>
+          <h1>Rock It Out</h1>
+          <button>Add a Route</button>
+          <button>View Your Routes</button>
+        </nav>
+        <div className='quote-div'>
+          <h1>Climb More, Worry Less</h1>
+        </div>
+      </main>
+    )
+  }
+}
+
+export default App
